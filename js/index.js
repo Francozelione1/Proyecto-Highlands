1ecf19ecb96f1eea94a816410ce03cf3d9dcf0b4
Franco Zelione Lenzi <francozelione796@gmail.com>
2023-12-31 07:15:58 -0300

    cursos, examenes mobile

--- js/index.js
+++ js/index.js
@@ -5,22 +5,22 @@ function ajustarContenido() {
     if (width <= 990) {
         document.getElementById("header").innerHTML = '<nav class="navbar navbar-expand-lg bg-body-tertiary navColor navbarNuestro">' +
         '<div class="container-fluid navColor">' +
-          '<a class="navbar-brand" href="./index.html">Inicio</a>' +
+          '<a class="navbar-brand" href="index.html">Inicio</a>' +
           '<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">' +
             '<span class="navbar-toggler-icon"></span>' +
           '</button>' +
           '<div class="collapse navbar-collapse" id="navbarSupportedContent">' +
             '<ul class="navbar-nav me-auto mb-2 mb-lg-0">' +
               '<li class="nav-item">' +
-                '<a class="nav-link" aria-current="page" href="./pages/contacto.html">Cursos</a>' +
+                '<a class="nav-link" aria-current="page" href="cursos.html">Cursos</a>' +
               '</li>' +
               '<li class="nav-item">' +
-                '<a class="nav-link" href="./pages/nosotros.html">Exámenes</a>' +
+                '<a class="nav-link" href="examenes.html">Exámenes</a>' +
               '</li>' +
-              '<a class="nav-link" href="./pages/nosotros.html">Nosotros</a>' +
+              '<a class="nav-link" href="nosotros.html">Nosotros</a>' +
               '</li>' +
               '</li>' +
-              '<a class="nav-link" href="./pages/nosotros.html">Contacto</a>' +
+              '<a class="nav-link" href="contacto.html">Contacto</a>' +
               '</li>' +
             '</ul>' +
           '</div>' +
